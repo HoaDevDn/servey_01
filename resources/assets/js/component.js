9240a755a5f601fc9a886e218b449d2dba791656
Phu Hoa <phuhoadn95@gmail.com>
2017-03-13 11:09:16 +0700

    fix migrate 2

--- resources/assets/js/component.js
+++ resources/assets/js/component.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
         var js, fjs = d.getElementsByTagName(s)[0];
         if (d.getElementById(id)) return;
         js = d.createElement(s); js.id = id;
-        js.src = "//connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v2.8&appId=1853033838292892";
+        js.src = "//connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v2.8&appId=738459509656428";
         fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));
 
